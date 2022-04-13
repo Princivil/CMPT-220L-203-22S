8394d9d2234aeaed2cd6398ea12b08ade66bf3b0
Princivil <79266103+Princivil@users.noreply.github.com>
2022-04-13 12:28:27 -0400

    Lab 6 - Plant Object

--- JavaLabs/Lab 6/src/base/Main.java
+++ JavaLabs/Lab 6/src/base/Main.java
@@ -1,4 +1,8 @@
 package base;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
 public class Main {
     /**
@@ -22,11 +26,45 @@ public class Main {
      * - Print out the Linear Data Types
      */
     public static void main(String[] args) {
+
         // Create an Array, Populate it, and Print it out
+        String[] cats = {"Tigger", "Foxy", "Batman", "Trixie"};
+        System.out.println("These are my cats:" + cats);
+
         // Create an ArrayList, Populate it, and Print it out
-        // Create an LinkedList, Populate it, and Print it out
-        // Create an Queue, Populate it, and Print it out
-        // Create an Stack, Populate it, and Print it out
+        ArrayList<String> pokemon = new ArrayList<>();
+        pokemon.add("Mewtwo");
+        pokemon.add("Zoroark");
+        pokemon.add("Ludia Legend");
+        pokemon.add("Pikachu");
+        System.out.println("Top 4 pokemon cards:" + pokemon);
+
+        // Create a LinkedList, Populate it, and Print it out
+        LinkedList<> friends = new LinkedList<>();
+        System.out.println(friends + "Oh, wait I have none.");
+
+        // Create a Queue, Populate it, and Print it out
+        Queue<> q = new LinkedList<>();
+        for(int i=0; 1<5; i++){
+            q.add(i);
+        }
+        
+
+        // Create a Stack, Populate it, and Print it out
+        Stack<> cards = new Stack<>();
+        cards.push.push("Ace of Hearts");
+        cards.push("King of Spades");
+        cards.push("Queen of Diamonds");
+        cards.push("Jack of Clovers");
+
+        System.out.println("First Pick:" + cards.pop() );
+        System.out.println("Deck:" + cards);
+
+
         // Using the information in the Lab 6 Jupyter Notebook create an instance of plant here.
+        Plant p1 = new Plant(true, "Lily", "Onion");
+        Plant p2 = new Plant(true, "Nightshade", "Tomato" );
+        System.out.println(p1);
+        System.out.println(p2);
     }
 }
---
+++ JavaLabs/Lab 6/src/base/Plant.java
@@ -0,0 +1,48 @@
+package base;
+
+public class Plant {
+    boolean needsWatering;
+    String plantFamily;
+    String name;
+
+
+    public Plant(boolan needsWatering, String plantFamily, String name){
+        this.needsWatering = true;
+        this.plantFamily = plantFamily;
+        this.name = name;
+        wateringInstructions();
+        sunlightRequirements();
+    }
+
+    public void wateringInstructions(){
+        if (plantFamily.equals("Legume")){
+            System.out.println("Drown me in water daily.");
+        }
+        else if (plantFamily.equals("Grass")){
+            System.out.println("Water me until my soil can't absorb no more. But don't over do it!");
+        }
+        else if (plantFamily.equals("Lily")){
+            System.out.println("Just keep my soil moist.");
+        }
+        else if (plantFamily.equals("Composite")){
+            System.out.println("Water me daily, but I can survive drought in case you forget.");
+        }
+        
+        else if (plantFamily.equals("Gourd")){
+            System.out.println("Water me heavily in the summer, but leave me be once I mature on my vines.");
+        }
+        else{
+            System.out.println("Drown my soil, then drain. Keeping me moist is essential.");
+        }    
+    }
+
+    public void sunlightRequirements(){
+        if (plantFamily.equals("Nightshade")){
+            System.out.println("Partial-shade will do, but would love full sun so I can continue to bloom at night.");
+        }
+        else{
+            System.out.println("Full sun only!");
+        }    
+    }
+    
+}
