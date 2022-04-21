8b6513a40b4a295769e0daec8f5e32ec65674728
Princivil <79266103+Princivil@users.noreply.github.com>
2022-04-21 17:17:24 -0400

    Lab 8 - Created

---
+++ JavaLabs/Lab 8/src/base/Main.java
@@ -0,0 +1,10 @@
+package base;
+
+public class Main {
+/* Copy list from lab 7
+* Use function to add to the end of the list
+*              ...delete things from the list.
+*              ...return reordered list.
+*/
+
+}
